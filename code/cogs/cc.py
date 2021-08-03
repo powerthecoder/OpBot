dad8801d82cb7d787c0c8f65c82e8bc1e4dd8ced
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-08-03 15:34:06 -0600

    Create cc.py

---
+++ code/cogs/cc.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import time
+import random
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from discord import Member
+from discord.ext.commands import has_permissions
+from discord.ext.commands import has_role
+from discord.ext.commands import MissingPermissions
+from discord.utils import find
+from discord.utils import get
+import asyncio
+import json
+
+class Main(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def cc(self, ctx, time, date, *,args):
+        embed=discord.Embed(title="Carrier Command", description=f"**Time:** {time}CST \n**Date:** {date} \n\n**Mission Description:** \n{args} \nReact to participate")
+        await ctx.send("<@&871957395436367872>")
+        msg1 = await ctx.send(embed=embed)
+        await msg1.add_reaction('✅')
+
+def setup(client):
+    client.add_cog(Main(client))
