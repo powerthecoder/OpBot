80d9bb25539af319a7dd45f1d0cbb09d937d868a
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-08-03 15:34:28 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import random
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from discord import Member
+from discord.ext.commands import has_permissions
+from discord.ext.commands import has_role
+from discord.ext.commands import MissingPermissions
+from discord.utils import find
+from discord.utils import get
+import asyncio
+import json
+
+class Main(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+def setup(client):
+    client.add_cog(Main(client))
