9d2b91b63598bdbbfe343c62ea045269881bf0a5
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-08-03 15:43:40 -0600

    Create about.py

---
+++ code/cogs/about.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+import random
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from discord import Member
+from discord.ext.commands import has_permissions
+from discord.ext.commands import has_role
+from discord.ext.commands import MissingPermissions
+from discord.utils import find
+from discord.utils import get
+import asyncio
+import json
+
+class Main(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def about(self, ctx):
+        embed=discord.Embed(title="Discord Bot Developer", description="**Developer:** <@291360056002215937> \n**Name:** Leo Power \n**GitHub**: https://github.com/powerthecoder", color=self.color)
+        embed.add_field(name="Discord Bot Developer", value="**Developer:** <@291360056002215937> \n**Name:** Leo Power", inline=False)
+        #embed.add_field(name="Steam Status Developer", value="**Developer:** xPaw \n**Name:** Pavel Djundik \n**Website:** https://xpaw.me/", inline=False)
+        embed.set_author(name="About")
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Main(client))
