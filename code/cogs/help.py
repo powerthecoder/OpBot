e15ec7aad37c251a854d8ad71140e92a5e7bea61
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-08-03 23:07:32 -0600

    Update help.py

--- code/cogs/help.py
+++ code/cogs/help.py
@@ -20,7 +20,7 @@ class Main(commands.Cog):
     
     @commands.command()
     async def help(self,ctx):
-        embed=discord.Embed(title="Carrier Command", description=f"**Help Menu** \n\n**Command Ussage**:\n-game <time> <date> <description>\nComamnd Example:\n-dcs 2000 1/2/3 Mission Description \n\n\n**Games:**\n Arma\n DCS\n Starbase\n stationeersn\n cotw (Hunter Call of the wild)\n Wolfpack\n cc (Carrier Command)")
+        embed=discord.Embed(title="Carrier Command", description=f"**Help Menu** \n\n**Command Ussage**:\n-game <time> <date> <description>\nComamnd Example:\n-dcs 2000 1/2/3 Mission Description \n\n\n**Games:**\n Arma\n DCS\n Starbase\n stationeers\n cotw (Hunter Call of the wild)\n Wolfpack\n cc (Carrier Command)")
         msg1 = await ctx.send(embed=embed)
 
 def setup(client):
