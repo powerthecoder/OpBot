9ee4285ea9bfa0257db20933819bad0f5da31a0f
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-08-03 23:07:29 -0600

    Update DCS.py

--- code/cogs/DCS.py
+++ code/cogs/DCS.py
@@ -20,10 +20,11 @@ class Main(commands.Cog):
     
     @commands.command()
     async def DCS(self, ctx, time, date, *,args):
-        embed=discord.Embed(title="DCS Digital Combat Simulator", description=f"**Time:** {time}CST \n**Date:** {date} \n\n**Mission Description:** \n{args} \nReact to participate")
+        embed=discord.Embed(title="DCS Digital Combat Simulator", description=f"**Time:** {time}CST \n**Date:** {date} \n**Organizer:** <@{ctx.message.author.id}>\n\n**Mission Description:** \n{args} \n\nReact ✅ to participate")
         await ctx.send("<@&871957137587314761>")
         msg1 = await ctx.send(embed=embed)
         await msg1.add_reaction('✅')
+        await ctx.message.delete()
 
 def setup(client):
     client.add_cog(Main(client))
